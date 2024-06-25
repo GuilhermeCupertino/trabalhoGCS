c45f57336cc7c5f00d287bf993d6b79fc5b08af3
Jecelen <jecelen.campos@ufms.br>
2024-06-25 09:31:56 -0400

    feat: adiciona validação ao campo estado no cadastro de novas cidades. Resolves issue #19

--- app/src/main/java/com/example/p2/activities/NovaCidade.java
+++ app/src/main/java/com/example/p2/activities/NovaCidade.java
@@ -6,25 +6,19 @@ import androidx.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.p2.R;
 import com.example.p2.database.AppDatabase;
 import com.example.p2.entities.Cidade;
 
-import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
-import com.google.android.libraries.places.api.model.AutocompletePrediction;
-import com.google.android.libraries.places.api.model.FindAutocompletePredictionsRequest;
-import com.google.android.libraries.places.api.model.FindAutocompletePredictionsResponse;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.net.PlacesClient;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.Status;
-
 public class NovaCidade extends AppCompatActivity {
-    private EditText nomeEditText, estadoEditText;
+    private EditText nomeEditText;
+    private Spinner estadoSpinner;
     private Button voltar, salvarButton;
     private AppDatabase db;
 
@@ -32,12 +26,15 @@ public class NovaCidade extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nova_cidade);
-        // Initialize Places
-        Places.initialize(getApplicationContext(), YOUR_API_KEY);
-        PlacesClient placesClient = Places.createClient(this);
 
         nomeEditText = findViewById(R.id.desc);
-        estadoEditText = findViewById(R.id.lat);
+        estadoSpinner = findViewById(R.id.estadoSpinner);
+
+        // Preenche o Spinner com os estados
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+            R.array.estados_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        estadoSpinner.setAdapter(adapter);
 
         db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "AppDatabase")
                 .allowMainThreadQueries()
@@ -60,78 +57,22 @@ public class NovaCidade extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-    }
-
-        // Método para verificar se a cidade existe utilizando a API do Google Places
-    private void cidadeExiste(String nomeCidade) {
-        AutocompleteSessionToken token = AutocompleteSessionToken.newInstance();
-
-        FindAutocompletePredictionsRequest request = FindAutocompletePredictionsRequest.builder()
-                .setTypeFilter(FindAutocompletePredictionsRequest.TypeFilter.CITIES)
-                .setQuery(nomeCidade)
-                .setSessionToken(token)
-                .build();
-
-        placesClient.findAutocompletePredictions(request).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                FindAutocompletePredictionsResponse response = task.getResult();
-                List<AutocompletePrediction> predictions = response.getAutocompletePredictions();
-
-                boolean cidadeEncontrada = false;
-
-                for (AutocompletePrediction prediction : predictions) {
-                    if (prediction.getPrimaryText(null).toString().equalsIgnoreCase(nomeCidade)) {
-                        cidadeEncontrada = true;
-                        break;
-                    }
-                }
-
-                if (cidadeEncontrada) {
-                    salvarCidade(nomeCidade);
-                } else {
-                    Toast.makeText(GerenciarCidades.this, "Cidade não encontrada", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(GerenciarCidades.this, "Erro ao verificar cidade: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(e -> {
-            if (e instanceof ApiException) {
-                ApiException apiException = (ApiException) e;
-                Status status = apiException.getStatus();
-                Toast.makeText(GerenciarCidades.this, "Erro ao verificar cidade: " + status.getStatusMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     private void salvarCidade() {
-    String nome = nomeEditText.getText().toString();
-    String estado = estadoEditText.getText().toString();
-    if(nome != null && !nome.trim().isEmpty() && estado != null && !estado.trim().isEmpty()) {
-        CidadeValidator cidadeValidator = new CidadeValidator(this);
-        cidadeValidator.validarCidade(nome + ", " + estado, new CidadeValidator.CidadeValidationListener() {
-            @Override
-            public void onValidationResult(boolean isValid) {
-                if (isValid) {
-                    Cidade novaCidade = new Cidade();
-                    novaCidade.setNomeCidade(nome);
-                    novaCidade.setEstado(estado);
+        String nome = nomeEditText.getText().toString();
+        String estado = estadoSpinner.getSelectedItem().toString();
+        if (nome != null && !nome.trim().isEmpty()) {
+            Cidade novaCidade = new Cidade();
+            novaCidade.setNomeCidade(nome);
+            novaCidade.setEstado(estado);
 
-                    db.cidadeDao().insert(novaCidade);
-                    Toast.makeText(NovaCidade.this, "Cidade salva com sucesso!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(NovaCidade.this, GerenciarCidades.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(NovaCidade.this, "Cidade não encontrada. Por favor, verifique os dados.", Toast.LENGTH_SHORT).show();
-                    logger.error("Tentativa de salvar cidade não existente: nome='{}', estado='{}'", nome, estado);
-                }
-            }
-        });
-    } else {
-        Toast.makeText(this, "Nome e estado da cidade não podem ser vazios.", Toast.LENGTH_SHORT).show();
-        logger.error("Tentativa de salvar cidade com dados faltantes: nome='{}', estado='{}'", nome, estado);
+            db.cidadeDao().insert(novaCidade);
+            Toast.makeText(NovaCidade.this, "Cidade salva com sucesso!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(NovaCidade.this, GerenciarCidades.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Nome e estado da cidade não podem ser vazios.", Toast.LENGTH_SHORT).show();
+        }
     }
-
- }
-
-
+}
